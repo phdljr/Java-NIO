cc22b867fc6aaac9212ec9c86f9605cbab3e629c
phdljr <phdljr@naver.com>
2022-01-30 18:52:21 +0900

    push

--- src/StudyPathClass.java
+++ src/StudyPathClass.java
@@ -4,35 +4,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class StudySeveralClass {
+public class StudyPathClass {
 	public static void main(String[] args) {
 		Path path = Paths.get("C:/test/cc");
-		//System.out.println(Files.isDirectory(path));
-		
-		if(Files.notExists(path)) { //해당 경로가 존재하지 않다면
+		// System.out.println(Files.isDirectory(path));
+
+		if (Files.notExists(path)) { // 해당 경로가 존재하지 않다면
 			System.out.println("존재하지 않음으로 생성");
 			try {
-				Files.createDirectories(path); //경로상에 존재하지 않는 모든 디렉토리 생성
+				Files.createDirectories(path); // 경로상에 존재하지 않는 모든 디렉토리 생성
 			} catch (IOException e) {
 				e.printStackTrace();
-			} 
+			}
 		}
-		
+
 		path = Paths.get("C:/test/hello.txt");
-		if(Files.notExists(path)) { //해당 경로가 존재하지 않다면
+		if (Files.notExists(path)) { // 해당 경로가 존재하지 않다면
 			System.out.println("존재하지 않음으로 생성");
 			try {
-				Files.createFile(path); //경로상에 존재하지 않는 모든 파일 생성
+				Files.createFile(path); // 경로상에 존재하지 않는 모든 파일 생성
 			} catch (IOException e) {
 				e.printStackTrace();
-			} 
+			}
 		}
-		
+
 		path = Paths.get("C:/test");
 		DirectoryStream<Path> directoryStream;
 		try {
 			directoryStream = Files.newDirectoryStream(path);
-			for(Path p : directoryStream) {
+			for (Path p : directoryStream) {
 				System.out.println(p.getFileName());
 			}
 		} catch (IOException e) {
